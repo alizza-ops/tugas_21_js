abd5c06d0d96c5401c0618af24eca408f6ceab71
alizza-ops <52742283+alizza-ops@users.noreply.github.com>
2019-11-16 00:09:16 +0700

    Add files via upload

---
+++ tugas21.js
@@ -0,0 +1,8 @@
+function rubahKata(){
+		var string1 = "Saya belajar masak nasi goreng";
+		console.log(string1);
+		var string2 = string1.replace("masak","makan")
+		return string2;
+}
+
+console.log(rubahKata());
